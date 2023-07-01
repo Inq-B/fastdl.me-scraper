b27f1f89c5a2e206bfc66aefcc999458d9c742c6
Inq <38569387+Inq-B@users.noreply.github.com>
2023-07-01 17:53:05 +0100

    add program and full list of bhop maps

---
+++ main.py
@@ -0,0 +1,26 @@
+#/usr/bin/python3
+
+import wget
+import os
+
+path = os.getcwd()+"/maps"
+maps = open(os.getcwd()+"/allbhopmaps.txt", "r")
+maplines = maps.read().split("\n")
+while True:
+    user = input(">>")
+    try:
+        user = int(user)
+        print(maplines[user])
+    except ValueError:
+        if user == "scrape":
+            for i in range(0,len(maplines)):
+                wget.download("http://main.fastdl.me/maps/"+maplines[i]+".bsp.bz2",out=path)
+                print("\n")
+                break
+                i+=1
+            break
+        else:
+            print("Invalid selection, exiting..")
+            break
+
+
